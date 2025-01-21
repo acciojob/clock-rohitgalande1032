2568b4db624e5a4630e926659de2f30e580a4cd1
Rohit Galande <80148975+rohitgalande1032@users.noreply.github.com>
2025-01-21 20:22:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+let timerDisplay = document.getElementById("timer");
+
+setInterval(function(){
+	timerDisplay.innerHTML = new Date().toLocaleString();
+}, 1000)
+
